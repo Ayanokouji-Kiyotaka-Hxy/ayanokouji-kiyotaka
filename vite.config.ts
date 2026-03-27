2eb7182f9196a21d0bd99bb64846f1afdf4b1a44
潇阳 黄 <hmap-hxy@outlook.com>
2026-03-27 15:11:59 +0800

    chore: 优化构建输出文件结构
    - 配置 manualChunks 拆分第三方依赖
    
    - 自定义 entry/chunk/asset 文件名格式
    
    - 新增 .gitignore 包含文件

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,18 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'static/js/[name].js', // 自定义入口文件名
+        chunkFileNames: 'static/js/[name].js', // 自定义 chunk 文件名
+        assetFileNames: 'static/[ext]/[name].[ext]', // 静态资源文件名
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+      },
+    },
+  },
 })
